67c1e0c8dbbc5d06a6df8dc5dcbcc95dde309031
Antoine Aube <aaube3@gmail.com>
2023-11-29 17:46:43 -0700

    db: add first user table

---
+++ db/migrate/20231130003716_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :address
+      t.float :lat
+      t.float :lon
+      t.boolean :is_remote
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
