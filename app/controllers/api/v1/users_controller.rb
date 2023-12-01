ba72f40be491fd8fc9f729cbc83a7d7fb1c8aec3
Antoine Aube <aaube3@gmail.com>
2023-11-30 21:41:50 -0700

    feature: create user action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,20 @@ class Api::V1::UsersController < ApplicationController
   def show
     render json: UserSerializer.new(User.find(params[:id]))
   end
+
+  def create
+    user = User.create(user_params)
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    else
+      render json: {error: user.errors.full_messages.to_sentence}
+    end 
+  end
+
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :address, :lat, :lon, :is_remote)
+  end
 end
