d718c896c603306c530163f561eed6338d5aaccd
Antoine Aube <aaube3@gmail.com>
2023-11-30 21:41:28 -0700

    models: add more validations in the event someone doesn't fill out the form correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :address, :lat, :lon, :is_remote, presence: true
   validates :email, presence: true, uniqueness: true
   has_many :user_meetings
   has_many :meetings, through: :user_meetings
