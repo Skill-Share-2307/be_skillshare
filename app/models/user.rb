e0953f09811909e2d0899270073af97f6939ce4f
Antoine Aube <aaube3@gmail.com>
2023-11-30 22:18:56 -0700

    fix: put is_remote on separate validation, it was causing the issue with the original test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, :address, :lat, :lon, :is_remote, presence: true
+  validates :first_name, :last_name, :address, :lat, :lon, presence: true
+  validates :is_remote, inclusion: { in: [true, false] }
   validates :email, presence: true, uniqueness: true
   has_many :user_meetings
   has_many :meetings, through: :user_meetings
