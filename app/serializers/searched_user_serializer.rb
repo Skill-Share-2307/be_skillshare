17f369ad6d85cc70aa38da7fdb058bd8902431ad
Antoine Aube <aaube3@gmail.com>
2023-12-02 09:53:18 -0700

    searched user serializer with placeholder for proximity

---
+++ app/serializers/searched_user_serializer.rb
@@ -0,0 +1,18 @@
+class SearchedUserSerializer
+include JSONAPI::Serializer
+
+  attributes :first_name, :last_name, :skills
+
+  attribute :skills do |user|
+    user.skills.map do |skill|
+      {
+        name: skill.name,
+        proficiency: skill.proficiency
+      }
+    end
+  end
+
+  #placeholder to render the proximity of the user to the searcher
+  #how are we going to get the lat/lon of the searcher? 
+  # attribute :proximity
+end
