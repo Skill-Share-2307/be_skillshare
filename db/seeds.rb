c5e0897c4b2f5c5fb7c9f32467a03adf24bf76fb
Antoine Aube <aaube3@gmail.com>
2023-12-02 10:10:06 -0700

    add a couple seeds to test with postman

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+
+user1 = User.create(first_name: "Steve", last_name: "Jobs", email: "steve@gmail.com", address: "12345 street st.", lat: "1.12", lon: "1.12", is_remote: "true", about: "I am a very good programmer")
+user2 = User.create(first_name: "Ethan", last_name: "Bustamante", email: "Ethan@gmail.com", address: "Ethan street st.", lat: "1.12", lon: "1.12", is_remote: "true", about: "I am a also very good programmer")
+
+steveskill1 = Skill.create(name: "Apple", proficiency: 5, user_id: user1.id)
+steveskill2 = Skill.create(name: "Swift", proficiency: 5, user_id: user1.id)
+
+ethanskill1 = Skill.create(name: "Ruby", proficiency: 5, user_id: user2.id)
+ethanskill2 = Skill.create(name: "Rails", proficiency: 5, user_id: user2.id)
