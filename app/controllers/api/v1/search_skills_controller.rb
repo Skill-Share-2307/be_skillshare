239aad465d0439c62a15587e93a84db4f035f7f5
Antoine Aube <aaube3@gmail.com>
2023-12-02 12:01:55 -0700

    feature: search for skill index with error handling for both no users with skill, or empty search

---
+++ app/controllers/api/v1/search_skills_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::SearchSkillsController < ApplicationController
+  before_action :searched_users, only: [:index]
+  before_action :check_for_empty_query
+
+  def index
+    if !@users.empty?
+      render json: SearchedUserSerializer.new(@users), status: 200
+    elsif searched_users.empty?
+      render json: {error: "No users found with that skill."}, status: 200
+    end 
+  end
+
+ private
+
+  def searched_users 
+    @users = User.search_for_skills(params[:query])
+  end
+
+  def check_for_empty_query
+    if params[:query].blank?
+      render json: {error: "Please enter a skill to search for."}, status: 400
+    end
+  end
+
+end
