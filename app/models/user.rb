e8252cb12a7e5e2cab31dd162c19396824bafc3c
Antoine Aube <aaube3@gmail.com>
2023-12-02 12:40:04 -0700

    refactor: update users model to have validations for new address attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, :address, :lat, :lon, presence: true
+  validates :first_name, :last_name, :lat, :lon, presence: true
+  validates :street, :city, :state, :zipcode, presence: true
   validates :is_remote, inclusion: { in: [true, false] }
   validates :email, presence: true, uniqueness: true
   has_many :user_meetings
