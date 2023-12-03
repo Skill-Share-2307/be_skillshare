962d96522b2192b6ad31b454956a2aedac720b10
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-12-02 20:11:06 -0700

    Add create route for meetings

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show, :create]
       resources :search_skills, only: [:index]
+      resources :meetings, only: [:create]
     end
   end
 end
