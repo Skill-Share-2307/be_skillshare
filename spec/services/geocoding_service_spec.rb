718836bf0ff3e85969953c615db608ac23099966
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-12-02 18:49:02 -0700

    add geocoding service to handle getting coords from an address

--- spec/services/geocoding_service_spec.rb
+++ spec/services/geocoding_service_spec.rb
@@ -10,18 +10,16 @@ RSpec.describe GeocodingService do
   describe "#geocode_address" do
     it "returns the geocoding of the provided address" do
       stub = File.read('spec/fixtures/geocoding_response.json')
-      stub_response(:get, "https://api.geoapify.com/v1/geocode/search?text=5935 Dublin Blvd #100, 
-      Colorado Springs, CO 80923&format=json&apiKey=#{Rails.application.credentials.geoapify[:api_key]}").
-        with_response(status: 200, body: stub)
+      stub_request(:get, "https://api.geoapify.com/v1/geocode/search?apiKey=#{Rails.application.credentials.geoapify[:api_key]}&format=json&text=5935%20Dublin%20Blvd%20%23100,%20Colorado%20Springs,%20CO%2080923").
+        to_return(status: 200, body: stub)
 
       
       response = GeocodingService.new.geocode_address("5935 Dublin Blvd #100, Colorado Springs, CO 80923")
-      geocode = JSON.parse(response, symbolize_names: true)
-      expect(geocode).to be_a Hash
-      expect(geocode).to have_key(:results)
-      expect(geocode[:results]).to be_an Array
+      expect(response).to be_a Hash
+      expect(response).to have_key(:results)
+      expect(response[:results]).to be_an Array
 
-      geocode[:results].each do |result|
+      response[:results].each do |result|
         expect(result).to be_a Hash
         expect(result).to have_key(:lon)
         expect(result[:lon]).to be_a Float
