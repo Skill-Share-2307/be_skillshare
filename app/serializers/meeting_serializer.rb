77c181383dc976a5463ff48824c56e0608dfbd8e
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-12-04 14:48:18 -0700

    Fix: adjust serializers to follow json contract

--- app/serializers/meeting_serializer.rb
+++ app/serializers/meeting_serializer.rb
@@ -1,5 +1,5 @@
 class MeetingSerializer
 include JSONAPI::Serializer
   
-  attributes :date, :start_time, :end_time, :is_accepted, :purpose, :partner_id, :host_id
+  attributes :date, :start_time, :end_time, :is_accepted, :purpose, :partner_id, :user_id
 end
