84df0a5425e50410670d5361f1f8a894c1278cfd
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-12-04 14:49:40 -0700

    Add a update route for meeting

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show, :create]
       resources :search_skills, only: [:index]
-      resources :meetings, only: [:create]
+      resources :meetings, only: [:create, :update]
     end
   end
 end
