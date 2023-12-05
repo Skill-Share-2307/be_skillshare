f06fc4b203a1103c20277b8393427455c75ee985
Antoine Aube <aaube3@gmail.com>
2023-12-04 19:36:01 -0700

    feature: error handling for user not found in create skill method

--- app/controllers/api/v1/skills_controller.rb
+++ app/controllers/api/v1/skills_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::SkillsController < ApplicationController
 
   def create
-    user = User.find(params[:user_id])
-    params[:skills].each do |skill|
-      user.skills.create(name: skill[:name], proficiency: skill[:proficiency] )
-    end
+    begin
+      user = User.find(params[:user_id])
 
-    render json: UserSerializer.new(user)
+      params[:skills].each do |skill|
+        user.skills.create(name: skill[:name], proficiency: skill[:proficiency] )
+      end
+      render json: UserSerializer.new(user), status: 201
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found.' }, status: :not_found
+    end
   end
 end
