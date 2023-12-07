52a9143262fbbcf14fc8eee7486d035885dc2dee
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-12-07 13:46:29 -0700

    revert to user_meeting serializer, update controller

--- app/controllers/api/v1/users/meetings_controller.rb
+++ app/controllers/api/v1/users/meetings_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Users::MeetingsController < ApplicationController
     begin
       user = User.find(params[:user_id])
       meetings = user.meetings.map {|meeting| MeetingPoro.new(meeting, user.id)}
-      render json: MeetingSerializer.new(meetings)
+      render json: UserMeetingSerializer.new(meetings)
     rescue ActiveRecord::RecordNotFound
       render json: { error: 'User not found.' }, status: :not_found
     end
