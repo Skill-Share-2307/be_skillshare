c1a446319414985ce397670ee11a624ac69748ba
Jorja Fleming <aset284@gmail.com>
2024-01-08 09:25:10 -0700

    removed unused code

--- app/controllers/api/v1/search_skills_controller.rb
+++ app/controllers/api/v1/search_skills_controller.rb
@@ -13,15 +13,6 @@ class Api::V1::SearchSkillsController < ApplicationController
 
   private
 
-  # This is a placeholder method that could be used if we want to add additional filters
-  # It would need to be used in the index method and replace the conditional that 
-  # currently checks for the params[:is_remote]
-  # def apply_filters(users)
-  #   users = users.remote_users if params[:users]
-  #   # Can add more filters after this to reduce the users array we get back.
-  #   users
-  # end
-
   def check_for_empty_query
     if params[:query].blank?
       render json: { error: "Please enter a skill to search for." }, status: 400
